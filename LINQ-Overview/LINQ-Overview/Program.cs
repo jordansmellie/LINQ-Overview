360c29c197cb1272845bf4b569fd6d2206432269
Jordan Smellie <jsmellie@gmail.com>
2014-07-17 11:40:24 -0400

    Created LINQ project

---
+++ LINQ-Overview/LINQ-Overview/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Overview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void NumQuery()
+        {
+            var numbers = new int[] { 1, 4, 9, 16, 25, 36 };
+            var evenNumbers = from p in numbers
+                              where (p % 2) == 0
+                              select p;
+        }
+    }
+}
